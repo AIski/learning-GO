0c1271123cb95c7929e6a96429fc63c437ffe443
AIski <102914351+AIski@users.noreply.github.com>
2024-12-02 21:30:01 +0100

    Used := syntax, as well multiple variables declaration in one line. Introduced constant of inflation.

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	const inflation float64 = 3
+	investmentAmount, years := 1000.0, 10.0
+	expectedReturnRate := 5.5
 
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	var futureRealValue = futureValue / math.Pow(1+inflation/100, years)
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
-	fmt.Printf("FeatureValue after %d months: %f\n.", years, futureValue)
+	fmt.Printf("FeatureValue after %d months: %f. \n", years, futureValue)
+	fmt.Printf("Real FeatureValue after %d months: %f. \n", years, futureRealValue)
 }
